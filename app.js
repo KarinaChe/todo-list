009d9ee2d1ea80d6af839751733dc710d947fe11
Artem <chernysh.karina221@gmail.com>
2023-06-01 16:06:11 +0300

    added nodemon

--- app.js
+++ app.js
@@ -158,10 +158,7 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-const port = process.env.PORT;
-if(port==null || port==""){
-  port = 3000
-}
-app.listen(port, function() {
-  console.log("Server started on port 3000");
-});
+const port = process.env.PORT || 3000;
+
+app.listen(port,() => 
+  console.log("Server started on port" + port));
